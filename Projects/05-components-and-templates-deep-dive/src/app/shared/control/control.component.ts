9e675e28294ca094d3f83530652219bffdc22c91
Ziad Ghoraba <Eng.ZiadGhoraba@gmail.com>
2026-08-03 02:17:45 +0300

    feat: enhance control component with click event handling and element width logging

--- Projects/05-components-and-templates-deep-dive/src/app/shared/control/control.component.ts
+++ Projects/05-components-and-templates-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,10 +8,24 @@ import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angula
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()'
   }
 })
 export class ControlComponent {
 
+  // @HostBinding('class') controlClass = 'control'; // This is not needed since we are using the host property in the component decorator
+  // @HostListener('click') onClick() { // This is not needed since we are using the host property in the component decorator
+  //   console.log('Control clicked');
+  // }
+
   label = input.required<string>();
+  private el = inject(ElementRef);
+
+
+  onClick() {
+    console.log('Control clicked');
+    const width = this.el.nativeElement.offsetWidth;
+    console.log('Control width:', width);
+  }
 }
