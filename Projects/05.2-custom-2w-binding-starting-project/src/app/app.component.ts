6c3b4f8c685bfc521b739e4b92ba9bf4b16bd9ce
Ziad Ghoraba <Eng.ZiadGhoraba@gmail.com>
2026-08-09 05:49:24 +0300

    feat: initialize custom two-way binding project with Angular 18
    - Added package.json with Angular dependencies and scripts.
    - Included favicon and various images in the public directory.
    - Created app component with input fields for rectangle dimensions.
    - Implemented rect component with custom two-way binding for size.
    - Added styles for the application and components.
    - Set up TypeScript configuration files for the project.
    - Created index.html and main.ts for application bootstrapping.

---
+++ Projects/05.2-custom-2w-binding-starting-project/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RectComponent } from './rect/rect.component';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [RectComponent, FormsModule],
+})
+export class AppComponent {
+  rectSize = signal<{ width: string; height: string }>({
+    width: '100',
+    height: '500',
+  });
+}
