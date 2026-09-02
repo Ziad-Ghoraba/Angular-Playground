fe74fe5cb60ea19b2a889463a551c43f2eacebd7
Ziad Ghoraba <Eng.ZiadGhoraba@gmail.com>
2026-09-02 14:02:59 +0300

    feat: add logging interceptor for HTTP requests and responses

--- Projects/11-http_requests_and_handeling_responses/src/main.ts
+++ Projects/11-http_requests_and_handeling_responses/src/main.ts
@@ -1,8 +1,33 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
+
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn)  {
+  // const clone = request.clone(
+  //   {
+  //     headers: request.headers.set('X-Custom-Header', 'MyCustomHeaderValue')
+  //   });
+  //   console.log('Outgoing request:', clone);
+  // return next(clone);
+  return next(request).pipe(
+    tap({
+      next: event => {
+        if(event.type == HttpEventType.Response) {
+          console.log('Incoming response:', event);
+        }
+      },
+      error: (error) => {
+        console.error('Error response:', error);
+      }
+    })
+  )
+
+}
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(
+    withInterceptors([loggingInterceptor])
+  )],
 }).catch((err) => console.error(err));
