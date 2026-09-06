2c37482a2967c686f1c2dfbeaff1a7b7276a70a3
Ziad Ghoraba <Eng.ZiadGhoraba@gmail.com>
2026-09-06 16:43:39 +0300

    feat(routing): add application routing configuration and define routes
    - Created app.config.ts to configure the Angular application with routing.
    - Added app.routes.ts to define application routes including user tasks and task management components.

---
+++ Projects/13-routing_and_building_multipage_single_page_applications/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Route } from "@angular/router";
+import { TasksComponent } from "./tasks/tasks.component";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+
+export const routes : Route[] = [
+    {
+        path: '', //<your-domain>
+        component: NoTaskComponent
+    },
+    {
+        path: 'users/:userId', //<your-domain>/tasks
+        component: UserTasksComponent,
+        children: [
+            {
+                path: 'tasks', //<your-domain>/users/:userId/tasks
+                component: TasksComponent
+            },
+            {
+                path: 'tasks/new', //<your-domain>/users/:userId/tasks/new
+                component: NewTaskComponent
+            }
+        ]
+    }
+]
--- Projects/13-routing_and_building_multipage_single_page_applications/src/app/users/user-tasks/user-tasks.component.ts
+++ Projects/13-routing_and_building_multipage_single_page_applications/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
+import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet],
 })
-export class UserTasksComponent {}
+export class UserTasksComponent implements OnInit {
+  userId = input.required<string>();
+  private usersService = inject(UsersService);
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+
+  userName = signal<string>('');
+
+  // userName = computed(() => {
+  //   const user = this.usersService.users.find(
+  //     (user) => user.id === this.userId()
+  //   );
+  //   return user ? user.name : '';
+  // });
+
+  ngOnInit(): void {
+    const subscription = this.activatedRoute.params.subscribe({
+      next: (params) => this.usersService.users.find((user) => {
+        if (user.id === params['userId']) {
+          this.userName.set(user.name);
+        }
+      })
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+}
